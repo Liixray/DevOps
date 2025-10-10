a5487294545d69f819787c4ae34f4997b623ee16
Nassim <nassim.mahnane@edu.esiee.fr>
2025-10-10 14:32:39 +0200

    Ajout de deux pages: inscription et connexion utilisateur

--- quiz-app/quiz-ui/src/router/index.js
+++ quiz-app/quiz-ui/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AdminLoginView from '@/views/AdminLoginView.vue'
+import UserLoginView from '@/views/UserLoginView.vue'
+import UserRegisterView from '@/views/UserRegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +16,17 @@ const router = createRouter({
       path: '/admin-login',
       name: 'admin-login',
       component: AdminLoginView,
-    }
+    },
+    {
+      path: '/user-login',
+      name: 'user-login',
+      component: UserLoginView,
+    },
+    {
+      path: '/user-register',
+      name: 'user-register',
+      component: UserRegisterView,
+    },
   ],
 })
 
--- quiz-app/quiz-ui/src/services/auth.js
+++ quiz-app/quiz-ui/src/services/auth.js
@@ -14,3 +14,31 @@ export async function loginAdmin(password) {
   }
   return (await res.json()).token
 }
+
+export async function registerUser({ name, mail, password }) {
+  const url = `${API_BASE}/auth/user/register`
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, mail, password }),
+  })
+  if (res.status !== 201) {
+    const txt = await res.text().catch(() => '')
+    throw new Error(txt || `HTTP ${res.status}`)
+  }
+  return await res.json()
+}
+
+export async function loginUser(mail, password) {
+  const url = `${API_BASE}/auth/user/login`
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ mail, password }),
+  })
+  if (!res.ok) {
+    const txt = await res.text().catch(() => '')
+    throw new Error(txt || `HTTP ${res.status}`)
+  }
+  return (await res.json()).token
+}
